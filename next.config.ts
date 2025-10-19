0478b5fe5cae52b0f38a397d27f658f28ed1b634
karlozrilic <karlozrilic@gmail.com>
2025-10-20 01:06:18 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
 		unoptimized: true, // Required for GitHub Pages
 	},
 	basePath: '', // if deploying to username.github.io
-	assetPrefix: '',
+	assetPrefix: ''
 };
 
 export default nextConfig;
