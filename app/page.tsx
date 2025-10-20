0ee847690b70771d44c8f3ecbbb5f57f1fb63eb3
karlozrilic <karlozrilic@gmail.com>
2025-10-20 14:14:59 +0200

    change

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,11 @@ export default function Home() {
     }, [dispatch]);
 
     useEffect(() => {
-        //console.log(data);
+        // console.log(data);
     }, [data]);
 
     useEffect(() => {
-        //console.log(loading);
+        // console.log(loading);
     }, [loading]);
 
     useEffect(() => {
