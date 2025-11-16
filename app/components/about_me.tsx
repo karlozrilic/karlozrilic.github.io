7c2d6798e3aee24df006b6ebccfe7348fd36f05f
karlozrilic <karlozrilic@gmail.com>
2025-11-17 00:05:41 +0100

    Added image

--- app/components/about_me.tsx
+++ app/components/about_me.tsx
@@ -14,7 +14,7 @@ export default function AboutMe() {
                 <h2 className='text-4xl font-bold text-center mb-10'>About Me</h2>
                 <div className='md:flex md:items-center md:space-x-10'>
                     <img
-                        src='https://media.licdn.com/dms/image/v2/D4D03AQE_2nSmVcB3uQ/profile-displayphoto-shrink_800_800/B4DZamj4cVGwAg-/0/1746551147030?e=1762387200&v=beta&t=GovCnmOQtt6VD8BNih7pqo51kMqjZf9E9Nz_L9N-3yY'
+                        src='/images/portfolio_picture.jpeg'
                         alt='Your Photo'
                         className='rounded-lg mb-6 md:mb-0 md:w-1/3 border-4 border-primary'
                     />
