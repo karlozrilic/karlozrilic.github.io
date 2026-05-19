340778e43ff9273435e09fb5cec5bb5dc72bcd56
Karlo Zrilić <44475533+karlozrilic@users.noreply.github.com>
2026-05-19 13:58:16 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
 		
 		        <Script
 		          src='/richtexteditor/plugins/all_plugins.js'
-		          strategy='beforeInteractive'
+		          strategy='afterInteractive'
 		        />
 		
 		        <Script
