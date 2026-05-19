c1fd60f84f139c24cc20bf67bd3cbd3931d05aae
Karlo Zrilić <44475533+karlozrilic@users.noreply.github.com>
2026-05-19 13:33:27 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -24,7 +24,6 @@ export default function Dashboard() {
 			const RichTextEditor = (window as any).RichTextEditor;
 
 			if (!RichTextEditor || !editorRef.current) return false;
-			console.log(RichTextEditor)
 
 			new RichTextEditor('#div_editor1', {
 				toolbar: 'default',
